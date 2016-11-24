5e333e8613899661a2dfacc6ff903071d701aec9
Charles <charleshung80@gmail.com>
2016-11-24 10:08:21 +0700

    Add runtime permission for compatibility in API level >=23 devices

--- app/src/main/java/com/example/charlessuresoft/barcodescanner_dev1/MainActivity.java
+++ app/src/main/java/com/example/charlessuresoft/barcodescanner_dev1/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.charlessuresoft.barcodescanner_dev1;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private Button scanBtn;
     private TextView formatTxt, contentTxt;
+    private static final int PERMISSIONS_REQUEST_CAMERA = 88;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +33,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         contentTxt = (TextView)findViewById(R.id.scan_content);
 
         scanBtn.setOnClickListener(this);
+
+        getPermissionCamera();
     }
 
     public void onClick(View v){
-//respond to clicks
+        //respond to clicks
+
         if(v.getId()==R.id.scan_button){
-//scan
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
@@ -50,8 +55,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             String scanFormat = scanningResult.getFormatName();
             formatTxt.setText("FORMAT: " + scanFormat);
             contentTxt.setText("CONTENT: " + scanContent);
-        }
-        else{
+        } else {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
@@ -59,5 +63,46 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    // permission was granted, Make SCAN flag to true.
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Permission Granted", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    // permission denied. Disable the functionality that depends on this permission.
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Permission Denied", Toast.LENGTH_SHORT);
+                    toast.show();
+                    ActivityCompat.finishAffinity(this);
+                }
+                return;
+            }
+            // other 'case' lines to check for other future permissions this app might request
+        }
+    }
 
+    private void getPermissionCamera(){
+        // check if permission already granted or not
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    PERMISSIONS_REQUEST_CAMERA);
+
+            // PERMISSIONS_REQUEST_CAMERA is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+    }
 }
