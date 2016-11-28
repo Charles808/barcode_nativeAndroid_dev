8546469d88d56d9f17d7eac7e2eb4c5d4ebaec70
Charles <charleshung80@gmail.com>
2016-11-28 11:54:00 +0700

    Change xml layout Change main activity code to match layout Add function to switch activity and parsing data

--- app/src/main/java/com/example/charlessuresoft/barcodescanner_dev1/MainActivity.java
+++ app/src/main/java/com/example/charlessuresoft/barcodescanner_dev1/MainActivity.java
@@ -2,31 +2,69 @@ package com.example.charlessuresoft.barcodescanner_dev1;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.BarcodeView;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+import java.util.List;
 
-    private Button scanBtn, openSiteBtn;
-    private TextView formatTxt, contentTxt;
-   // private static final int PERMISSIONS_REQUEST_CAMERA = 88;
+public class MainActivity extends AppCompatActivity {
+
+    private DecoratedBarcodeView barcodeView;
+    private String scanText, formatText;
+
+    /*private enum ParseResult {
+        URL,
+        VCARD,
+        EMAIL,
+        ETC
+    }*/
+
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null || result.getText().equals(scanText)) {
+                // Prevent duplicate scans
+                return;
+            }
+
+            scanText = result.getText();
+
+            barcodeView.setStatusText(result.getText());
+
+            //Added preview of scanned barcode
+            ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+
+            barcodeView.pause();
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,117 +72,51 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         setContentView(R.layout.activity_main);
 
-        scanBtn = (Button)findViewById(R.id.scan_button);
-        openSiteBtn = (Button)findViewById(R.id.openSite_button);
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        formatTxt.setText("Empty");
-        contentTxt = (TextView)findViewById(R.id.scan_content);
-        contentTxt.setText("Empty");
-
-        scanBtn.setOnClickListener(this);
-        openSiteBtn.setOnClickListener(this);
-
-        startScan();
-        //getPermissionCamera();
+        barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
+        barcodeView.decodeContinuous(callback);
     }
 
-    private void startScan() {
-        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-        scanIntegrator.initiateScan();
-    }
-
-    public void onClick(View v){
-        // Scan button action
-        if(v.getId()==R.id.scan_button){
-            //scanIntegrator.setBarcodeImageEnabled(true);
-            startScan();
-        }
-
-        else if(v.getId()==R.id.openSite_button){
-            //float url_size = contentTxt.getTextSize();
-            //Log.d("ADebugTag", "Value: " + Float.toString(url_size));
-            /*String url_addr = contentTxt.getText().toString();
-            if(url_addr != null) {
-                Log.d("ADebugTag", url_addr);
-                Uri uri = Uri.parse(url_addr); // missing 'http://' will cause crashed
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            } else {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "No URL Detected...", Toast.LENGTH_SHORT);
-                toast.show();
-            }*/
-
-        }
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        // Retrieve scan result
-        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        // A result received
-        String scanContent = scanningResult.getContents();
-        String scanFormat = scanningResult.getFormatName();
-        //String scanImagePath = intent.getStringExtra(Intents.Scan.RESULT_BARCODE_IMAGE_PATH);
-        formatTxt.setText("FORMAT: " + scanFormat);
-        contentTxt.setText("CONTENT: " + scanContent);
-    }
-    /* Using new library of ZXing 3.4.0 this won't be needed anymore....
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    // permission was granted, Make SCAN flag to true.
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "Permission Granted", Toast.LENGTH_SHORT);
-                    toast.show();
-                } else {
-                    // permission denied. Disable the functionality that depends on this permission.
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "Permission Denied", Toast.LENGTH_SHORT);
-                    toast.show();
-                    ActivityCompat.finishAffinity(this);
-                }
-                return;
-            }
-            // other 'case' lines to check for other future permissions this app might request
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void scanApp(View view) {
+        barcodeView.resume();
+    }
+
+    public void exitApp(View view) {
+        //barcodeView.resume();
+    }
+
+    private void switchActivity(String dataString) {
+
+        int parseResult = parseData(dataString);
+
+        switch (parseResult) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
         }
     }
 
-    private void getPermissionCamera(){
-        // check if permission already granted or not
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
-            // No explanation needed, we can request the permission.
-
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.CAMERA},
-                    PERMISSIONS_REQUEST_CAMERA);
-
-            // PERMISSIONS_REQUEST_CAMERA is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-        }
+    private int parseData(String rawString) {
+        return 0;
     }
-    */
-    // Experimental Function !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    /*
-    private void checkFlag() {
-        if(flag) {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "True", Toast.LENGTH_SHORT);
-            toast.show();
-        } else {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "False", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-    }
-    */
-    // Experimental Function !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
