47e4db89f2ebe38a17e4fc7db4e8549c07669697
Charles <charleshung80@gmail.com>
2016-11-29 14:59:18 +0700

    Add Copy button in Url activity Change layout in Url Activity

--- app/src/main/java/com/example/charlessuresoft/barcodescanner_dev1/UrlActivity.java
+++ app/src/main/java/com/example/charlessuresoft/barcodescanner_dev1/UrlActivity.java
@@ -1,5 +1,7 @@
 package com.example.charlessuresoft.barcodescanner_dev1;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -9,17 +11,20 @@ import android.widget.TextView;
 
 public class UrlActivity extends AppCompatActivity {
 
-    TextView urlView;
+    private TextView urlView;
+
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_url);
 
+        myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+
         urlView = (TextView) findViewById(R.id.urlText);
-
         String urlStr = getIntent().getStringExtra("DATA_STRING");
-
         urlView.setText(urlStr);
     }
 
@@ -42,5 +47,10 @@ public class UrlActivity extends AppCompatActivity {
         Uri uriUrl = Uri.parse(urlView.getText().toString());
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
+    }
+
+    public void copyApp (View view) {
+        myClip = ClipData.newPlainText("text", urlView.getText().toString());
+        myClipboard.setPrimaryClip(myClip);
     }
 }
